f99fd54b13858c379c8b32d4e85688c42087eea9
Joel_Bruno_Ferreira <1191843@isep.ipp.pt>
2023-10-29 18:13:31 +0000

    USBD07 SQL query and output result

--- docs/Sprint_1/US_BD_07/USBD07_SQL_Query.sql
+++ docs/Sprint_1/US_BD_07/USBD07_SQL_Query.sql
@@ -3,3 +3,15 @@
 
 -- SQL Query
 
+SELECT
+    TipoOperacao.DesignacaoTipoOperacao, COUNT(*) as NumberOfOperations
+FROM
+    Operacao
+INNER JOIN
+        Parcela ON Operacao.ParcelaID = Parcela.ParcelaID
+INNER JOIN
+        TipoOperacao ON Operacao.TipoOperacaoID = TipoOperacao.TipoOperacaoID
+WHERE
+    Parcela.Designacao = 'Vinha' AND Operacao.DataRealizacao BETWEEN TO_DATE('2022-01-01', 'YYYY-MM-DD')
+        AND TO_DATE('2023-09-15', 'YYYY-MM-DD')
+GROUP BY TipoOperacao.DesignacaoTipoOperacao;
