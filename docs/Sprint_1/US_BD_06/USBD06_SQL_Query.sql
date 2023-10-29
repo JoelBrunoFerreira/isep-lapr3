c3bd2bb2612c4fd72d9b24eb9974bca59f787b8a
Fabio Borges <1100719@isep.ipp.pt>
2023-10-29 17:44:39 +0000

    G311-50 [feature] USBD06 SQL query created

--- docs/Sprint_1/US_BD_06/USBD06_SQL_Query.sql
+++ docs/Sprint_1/US_BD_06/USBD06_SQL_Query.sql
@@ -3,3 +3,17 @@
 
 -- SQL Query
 
+SELECT
+    TipoFatorProducao.DescricaoTipoFatorProducao, COUNT(Operacao.OperacaoID) AS NumeroOperacoes
+    FROM
+    Operacao
+INNER JOIN
+    Parcela ON Operacao.ParcelaID = Parcela.ParcelaID
+INNER JOIN
+    FatorProducao ON Operacao.FatorProducaoID = FatorProducao.FatorProducaoID
+INNER JOIN
+    TipoFatorProducao ON FatorProducao.TipoFatorProducaoID = TipoFatorProducao.TipoFatorProducaoID
+WHERE
+    Operacao.DataRealizacao BETWEEN TO_DATE('2022-01-01', 'YYYY-MM-DD') AND TO_DATE('2023-09-15', 'YYYY-MM-DD')
+AND Parcela.Designacao = 'Vinha'
+GROUP BY TipoFatorProducao.DescricaoTipoFatorProducao
