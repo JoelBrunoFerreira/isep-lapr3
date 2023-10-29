b02a1e87a8559f016537f23f736cdc59bbd42780
rita <1180708@isep.ipp.pt>
2023-10-29 21:13:23 +0000

    G311-9 [feature] Adição script query USBD09

--- docs/Sprint_1/US_BD_09/USBD09_SQL_Query.sql
+++ docs/Sprint_1/US_BD_09/USBD09_SQL_Query.sql
@@ -2,4 +2,8 @@
 -- Como Gestor Agrícola, pretendo saber o número de aplicaçóes de cada tipo de factor de produção aplicados na instalação agricola num dado intervalo de tempo.
 
 -- SQL Query
-
+SELECT TipoFatorProducao.DescricaoTipoFatorProducao, COUNT(Operacao.OperacaoID) AS NumeroOperacoes FROM Operacao
+INNER JOIN FatorProducao ON Operacao.FatorProducaoID = FatorProducao.FatorProducaoID
+INNER JOIN TipoFatorProducao ON FatorProducao.TipoFatorProducaoID = TipoFatorProducao.TipoFatorProducaoID
+WHERE Operacao.DataRealizacao BETWEEN TO_DATE('data_inicio', 'YYYY-MM-DD') AND TO_DATE('data_fim', 'YYYY-MM-DD')
+GROUP BY TipoFatorProducao.DescricaoTipoFatorProducao
