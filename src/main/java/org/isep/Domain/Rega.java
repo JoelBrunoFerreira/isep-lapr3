a9a1994d9f0f7c257b11e830ce4f9e54bd203190
Victor Salgado <1221722@isep.ipp.pt>
2023-12-06 17:58:34 +0000

    Sprint3: USLP10 + USLP11 (pre-testing)

---
+++ src/main/java/org/isep/Domain/Rega.java
@@ -0,0 +1,84 @@
+package org.isep.Domain;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+public class Rega implements Comparable<Rega>, Serializable {
+    //private final int DIAS_PLANO = 30;
+    private LocalDate diaInicio;
+    private LocalDate diaFim;
+    private LocalTime horaInicioRega;
+    private LocalTime horaFimRega;
+    private Parcela parcela;
+    private Fertilizante fertilizante;
+
+    public LocalTime getHoraInicioRega() {
+        return horaInicioRega;
+    }
+
+    public LocalDate getDiaInicio() {
+        return diaInicio;
+    }
+
+    public LocalDate getDiaFim() {
+        return diaFim;
+    }
+
+    public Rega(LocalDate diaInicio, LocalTime horaInicioRega, LocalTime horaFimRega, Parcela parcela) {
+        this.diaInicio = diaInicio;
+        this.diaFim = diaInicio;//diaInicio.plusDays(1);
+        this.horaInicioRega = horaInicioRega;
+        this.horaFimRega = horaFimRega;
+        this.parcela = parcela;
+        this.fertilizante = null;
+    }
+
+    public Rega(LocalDate diaInicio, LocalTime horaInicioRega, LocalTime horaFimRega, Parcela parcela, Fertilizante fertilizante) {
+        this.diaInicio = diaInicio;
+        this.horaInicioRega = horaInicioRega;
+        this.horaFimRega = horaFimRega;
+        this.parcela = parcela;
+        this.fertilizante = fertilizante;
+    }
+
+    public Fertilizante getFertilizante() {
+        return fertilizante;
+    }
+
+    public LocalTime getHoraFimRega() {
+        return horaFimRega;
+    }
+
+    public Parcela getParcela() {
+        return parcela;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (!(object instanceof Rega rega)) return false;
+        return Objects.equals(diaInicio, rega.diaInicio) && Objects.equals(diaFim, rega.diaFim) && Objects.equals(horaInicioRega, rega.horaInicioRega) && Objects.equals(horaFimRega, rega.horaFimRega) && Objects.equals(parcela, rega.parcela);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(diaInicio, diaFim, horaInicioRega, horaFimRega, parcela);
+    }
+
+    @Override
+    public String toString() {
+        if (this.fertilizante == null) {
+            return String.format("%s, hora de início %s, hora de fim %s.", parcela, horaInicioRega, horaFimRega);
+        } else {
+            return String.format("%s, hora de início %s, hora de fim %s, mistura: %s.", parcela, horaInicioRega, horaFimRega, fertilizante.getMix());
+        }
+    }
+
+    @Override
+    public int compareTo(Rega o) {
+        return this.parcela.getSetor().compareTo(o.getParcela().getSetor());
+    }
+}
