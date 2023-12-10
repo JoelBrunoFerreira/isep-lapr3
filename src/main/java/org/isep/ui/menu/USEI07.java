69ea652e67b3fcd1a67f3fa1b7553d6a256cb62e
Joel_Bruno_Ferreira <1191843@isep.ipp.pt>
2023-12-10 20:15:12 +0000

    Menu Update

---
+++ src/main/java/org/isep/ui/menu/USEI07.java
@@ -0,0 +1,10 @@
+package org.isep.ui.menu;
+
+public class USEI07 {
+
+    public static void usei07Start() {
+
+        System.out.println("Not implemented yet....");
+
+    }
+}
---
+++ src/main/java/org/isep/ui/menu/USEI09.java
@@ -0,0 +1,10 @@
+package org.isep.ui.menu;
+
+public class USEI09 {
+
+    public static void usei09Start(){
+
+        System.out.println("Not implemented yet....");
+
+    }
+}
