2fc23b65e4af740f1562145885e803944bc38475
Victor Salgado <1221722@isep.ipp.pt>
2023-12-19 19:35:10 +0000

    USBD27 + USBD28(not final)

---
+++ docs/Sprint_3/US_BD/US_BD_27/usbd27_SQLquery.sql
@@ -0,0 +1,9 @@
+--USBD27
+--Não é possível alterar ou apagar os logs.
+
+CREATE OR REPLACE TRIGGER trgApagarOuAlterarLogs
+BEFORE DELETE OR UPDATE ON Log
+FOR EACH ROW
+BEGIN
+    RAISE_APPLICATION_ERROR(-20001, 'Não é permitido excluir ou atualizar registos de logs.');
+END;
