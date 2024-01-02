e783e61aae48837014507e1bcf2a147e183dc322
rita <1180708@isep.ipp.pt>
2024-01-02 23:24:34 +0000

    G311-137 [feature] Add US BD 25

--- docs/Sprint_3/US_BD/US_BD_25/usbd25_SQLquery.sql
+++ docs/Sprint_3/US_BD/US_BD_25/usbd25_SQLquery.sql
@@ -1,6 +1,6 @@
 -- USBD25 Como Gestor Agrícola, pretendo que a identificação da operação seja um número sequencial, não gerado automaticamente pelo SGBD, que deve ser gerado no contexto da transação de registo da operação. Se este registo falhar, não deve haver consequências, nomeadamente a existência de "buracos" na numeração.
 
-CREATE OR REPLACE TRIGGER trg_aumentar_operacao_id_before_insert
+CREATE OR REPLACE TRIGGER trg_incrementar_operacao_id_before_insert
 BEFORE INSERT ON Operacao
 FOR EACH ROW
 DECLARE
